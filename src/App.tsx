433c579f1804a352fd3c9e112142d51f5343715a
ChungTau <edwardonionc020903@gmail.com>
2023-10-23 14:26:23 +0800

    Update FacebookID

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
 import './App.css';
 import ShareToInstagramStory from './ShareToInstagramStory';
 
 function App() {
+  useEffect(() => {
+    // 載入 Facebook SDK
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      //@ts-ignore
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode?.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }, []);
   return (
     <div className="App">
       <h1>React App to Share to Instagram Stories</h1>
--- src/ShareToInstagramStory.tsx
+++ src/ShareToInstagramStory.tsx
@@ -8,17 +8,26 @@ interface Props {
 const ShareToInstagramStory: React.FC = () => {
   // ShareToInstagram.tsx
 
-  const [isSdkLoaded, setSdkLoaded] = useState(false);
-
   useEffect(() => {
-    if (window.FB) {
-      setSdkLoaded(true);
-    }
-  }, []);
+  // SDK 載入完成時會立即呼叫 fbAsyncInit，在這個函式中對 Facebook SDK 進行初始化
+  window.fbAsyncInit = function () {
+    // 初始化 Facebook SDK
+    window.FB.init({
+      appId: '762957345592162',
+      cookie: true,
+      xfbml: true,
+      version:'v18.0',
+    });
+
+    console.log('[fbAsyncInit] after window.FB.init');
+    window.FB.AppEvents.logPageView();
+  };
+
+
+}, []);
 
 const handleShare = () => {
-  if (isSdkLoaded) {
-
+  if (window.FB) {
     window.FB.ui({
       method: 'share',
       href: 'https://opengraph.githubassets.com/d8c280b965a9debe78481a78417528438a6eee0d2457ccc81fd6b61ed32055e9/appchoose/react-native-share-instagram-story'
@@ -27,8 +36,7 @@ const handleShare = () => {
     });
   } else {
     console.error('FB SDK not loaded yet');
-  
-}
+  }
 }
 
   return (
